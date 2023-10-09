48d528851c8ddbef5819317e850710e73cb71d55
Praecipua <git@mail.praecipua.org>
2023-10-09 14:38:09 +0900

    VideoURL-1

---
+++ VideoURL/iwaSync/player_iwaSync.cs
@@ -0,0 +1,41 @@
+﻿using HoshinoLabs.IwaSync3.Udon;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Praecipua.Udon.VideoURL
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class player_iwaSync : VideoControllerEventListener
+    {
+        public VideoCore videoCore;
+        public InputField inputField;
+        public bool useMessage;
+
+        private void Start()
+        {
+            if (videoCore == null)
+            {
+                Debug.LogError("iwaSync VideoCore is not set.");
+                return;
+            }
+
+            videoCore.AddListener(this);
+        }
+
+        public override void OnChangeURL()
+        {
+            string playingURL = videoCore.url.Get();
+            Debug.Log("URL changed. Currently playing URL: " + playingURL);
+
+            inputField.text = playingURL;
+
+            if (useMessage)
+            {
+                videoCore.Message = playingURL;
+            }
+        }
+    }
+}
